79853b82b5c6ebddc1783fdba0bbf06a545c0def
Safak <safak.inan61@hotmail.com>
2019-11-12 00:15:21 +0100

    Home page WIP, Fix images in responsiveness

---
+++ script.js
@@ -0,0 +1,14 @@
+var dropBtn;
+
+$(document).ready(function(){
+    dropBtn = document.getElementsByClassName('navbar-toggler')[0];
+});
+
+function toggleStyle() {
+    console.log("hfujoehfoh");
+    dropBtn.classList.toggle('button-drop');
+}
+
+
+
+
