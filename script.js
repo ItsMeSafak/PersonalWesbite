7aa26c0026c68e09384c0a9c951561f1b7da8db7
Safak <safak.inan61@hotmail.com>
2019-11-12 18:03:32 +0100

    Added smooth navigation

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ var dropBtn;
 
 $(document).ready(function(){
     dropBtn = document.getElementsByClassName('navbar-toggler')[0];
+    
 });
 
 function toggleStyle() {
     console.log("hfujoehfoh");
     dropBtn.classList.toggle('button-drop');
 }
+
+
 
 
 
